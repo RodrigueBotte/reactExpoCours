597e3c4397ac49a0dcb649e8e299480313571628
RodrigueBotte <rodrigue.botte@gmail.com>
2025-09-02 15:24:18 +0200

    expo 5

--- my-fisrt-app/app/(tabs)/index.tsx
+++ my-fisrt-app/app/(tabs)/index.tsx
@@ -1,38 +1,32 @@
 import { Image } from "expo-image";
 import { Platform, StyleSheet, Text } from "react-native";
+import { s } from "../../App.styles";
+import { FlexDemo } from "../../components/Flexdemo";
 
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import Human from "@/components/Human/Human";
+import { View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <ThemedView style ={{flex:1, justifyContent: "center", alignItems: "center"}}>
-      <Human />
-      <ThemedText>
-        Fin de l'écran
-      </ThemedText>
-    </ThemedView>
+    // <ThemedView
+    //   style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    // >
+    //   <Human />
+    //   <ThemedText>Fin de l'écran</ThemedText>
+    //   <View style={s.square}>
+    //     <Text>Mon carré stylé</Text>
+    //   </View>
+    // </ThemedView>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        
+        <FlexDemo/>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-});
---
+++ my-fisrt-app/components/FlexDemo.style.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+export const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  box1: {
+    height: 100,
+    width: 100,
+    backgroundColor: "red",
+  },
+  // box2: {
+  //   height: 800,
+  //   width: 100,
+  //   backgroundColor: "green",
+  // },
+  // box3: {
+  //   height: 800,
+  //   width: 100,
+  //   backgroundColor: "blue",
+  // },
+});
